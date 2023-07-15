38f226f7e7f1add3b069fabfb8f10b43d6dab16a
FricoSimon <simonfriko@gmail.com>
2023-07-15 16:50:53 +0700

    add explanation to simple search

--- simpleSearch.js
+++ simpleSearch.js
@@ -1,9 +1,9 @@
-const listItems = [1, 2, 3, 4, 5, 6, 7, 8, 9] // array numbers
+const listItems = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 // simple search function
 const simpleSearch = (list, item) => {
-    let index = 0 // starting index in array
-    let first = list[index] // call the index in array
+    let index = 0
+    let first = list[index]
 
     while (first <= item) {
         if (first === item) {
@@ -16,3 +16,13 @@ const simpleSearch = (list, item) => {
 }
 
 console.log(simpleSearch(listItems, 1));
+
+// first send an array
+// function will get array and the item to search for
+// find from first array item to last array item
+// check if item is less than or equal to the item to search for
+// if it is, check if it is equal to the item to search for
+// if it is, return the index
+// if it is not, increment the index and check the next item
+// if it is not, return "Not found"
+// the array should be sorted first 
